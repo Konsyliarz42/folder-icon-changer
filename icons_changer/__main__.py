be78e252c58ab8e5c3575ff48a968ce04a221b66
Tomasz Kordiak <dotomika@gmail.com>
2021-11-20 23:58:12 +0100

    Add example json

--- icons_changer/__main__.py
+++ icons_changer/__main__.py
@@ -2,8 +2,5 @@ from pathlib import Path
 from .iconChanger import IconChanger
 
 if __name__ == "__main__":
-    folder = Path("./test_folder")
-    icon = Path("./test_folder/test_icon.ico")
-
     chager = IconChanger()
-    chager.include_json("icons.json")
+    chager.include_json("example.json")
