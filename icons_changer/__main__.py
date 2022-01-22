4a4da0c11df1b4b7e3d05c88fcff32d474693e3a
Tomasz Kordiak <dotomika@gmail.com>
2022-01-22 23:06:34 +0100

    Fix manual setting icon

--- icons_changer/__main__.py
+++ icons_changer/__main__.py
@@ -1,14 +1,19 @@
 from argparse import ArgumentParser
 
-from .icon_changer import IconChanger
+from .icon_changer import Directory
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
+
     parser.add_argument("--folder", help="Path to folder.", required=True)
     parser.add_argument("--icon", help="Path to icon file.", default=None)
     parser.add_argument("--index", help="Icon index.", default=0, type=int)
+
     args = parser.parse_args()
-    changer = IconChanger()
-    folder = changer.add_folder(args.folder, args.icon, args.index)
-    changer.set_icon(folder)
+    folder = Directory(args.folder)
+
+    if args.icon:
+        folder.set_icon(args.icon, args.index)
+    else:
+        folder.remove_icon()
