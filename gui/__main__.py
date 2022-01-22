04a237db741304dc80005f2ce3f724fc5e195cfe
Tomasz Kordiak <dotomika@gmail.com>
2022-01-22 23:40:36 +0100

    Fun

--- gui/__main__.py
+++ gui/__main__.py
@@ -4,10 +4,14 @@ from PyQt6.QtWidgets import QApplication
 from .window import MainWindow
 
 
-if __name__ == "__main__":
+def run() -> None:
     app = QApplication(sys.argv)
 
     window = MainWindow()
     window.show()
 
     app.exec()
+
+
+if __name__ == "__main__":
+    run()
